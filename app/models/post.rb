c7bff2ca8b43848000fe9269ee7bd247cb584da9
brooklyndadcore <scott.waring@gmail.com>
2018-10-17 14:05:05 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  CAT_CAT = ["Fiction", "Non-Fiction"]
+  validates :title, presence: {message: "Title Is Required"}
+  validates :content, length: {minimum: 100, message: "The content is too short."}
+  validates :category, presence: true, inclusion: CAT_CAT
 end
